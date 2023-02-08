df1c9614d4278defa842024a3cf190a321235cc6
Mari Costa <mariana.luiza.s.a@gmail.com>
2023-02-08 08:51:01 -0300

    Projeto Pessoal

---
+++ src/main/java/org/generation/blogpessoal/BlogpessoalitauApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpessoalitauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpessoalitauApplication.class, args);
+	}
+
+}
